09c705eeed4691e44d3e03e713023cfd901b9c5d
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-01-12 21:23:28 -0600

    fixed reference tags, set-up array

---
+++ script.js
@@ -0,0 +1,10 @@
+// variables
+console.log('this is frm script.js')
+var question = ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5"];
+document.getElementById("demo").innerHTML = question[0];
+// array
+// var cars = ["Saab", "Volvo", "BMW"];
+//document.getElementById("demo").innerHTML = cars[0];
+
+
+// storage
