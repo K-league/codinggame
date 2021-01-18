17442c06b5aefe948719e18a1372b2c606011669
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-01-18 16:58:22 -0600

    added html radio tag to q form, added h1 and button style to css, edited for loop js

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 // variables
-var q1 = "Question 1"
-var q2 = "Question 2"
-var q3 = "Question 3"
-var q4 = "Question 4"
-var q5 = "Question 5"
+var q1 = document.querySelector("Question 1");
+var q2 = document.querySelector("Question 2");
+var q3 = document.querySelector("Question 3");
+var q4 = document.querySelector("Question 4");
+var q5 = document.querySelector("Question 5");
+
+
+
+var question = [];
+
 
 // add buttons to js
 
-console.log('this is frm script.js')
-var question = [q1, q2, q3, q4, q5];
-document.getElementById("demo").innerHTML = question[0];
 // array
+// var question = [q1, q2, q3, q4, q5];
+// document.getElementById("demo").innerHTML = question[q1];
+
+function renderQuestions() {
+    question.innerHTML= "";
+    question.textContent = question.length;
+    
+    for (var i = 0; i < question.length; i++) {
+        var question = question[i];
+        var li = document.createElement("li");
+        li.textContent.textContent = question;
+        li.setAttribute("data-index", i);
+
+        var button = document.createElement("li");
+        button.textContent = "Submit";
+
+
+    }
+}
 // var cars = ["Saab", "Volvo", "BMW"];
 //document.getElementById("demo").innerHTML = cars[0];
 
